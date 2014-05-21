fee6da048128c8cd92bbcceab302cc7933178a7e
Michael Babker <mbabker@flbab.com>
2014-05-21 12:28:48 -0500

    Refactor based on working BabDev\Renderer concept

--- src/AbstractHtmlView.php
+++ src/AbstractHtmlView.php
@@ -10,6 +10,7 @@ namespace Joomla\View;
 
 use Joomla\Filesystem\Path;
 use Joomla\Model\ModelInterface;
+use BabDev\Renderer\RendererInterface;
 
 /**
  * Joomla Framework HTML View Class
@@ -19,6 +20,14 @@ use Joomla\Model\ModelInterface;
 abstract class AbstractHtmlView extends AbstractView
 {
 	/**
+	 * The data array to pass to the renderer engine
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $data = array();
+
+	/**
 	 * The view layout.
 	 *
 	 * @var    string
@@ -27,38 +36,48 @@ abstract class AbstractHtmlView extends AbstractView
 	protected $layout = 'default';
 
 	/**
-	 * The view template engine.
-	 *
-	 * @var    RendererInterface
-	 * @since  __DEPLOY_VERSION__
-	 */
-	protected $renderer = null;
-
-	/**
 	 * The paths queue.
 	 *
 	 * @var    \SplPriorityQueue
 	 * @since  1.0
-	 * @deprecated  3.0  Use the path methods of RendererInterface instead
+	 * @deprecated  3.0  Set the paths in the RendererInterface object instead
 	 */
 	protected $paths;
+
+	/**
+	 * The renderer object
+	 *
+	 * @var    RendererInterface
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $renderer;
 
 	/**
 	 * Method to instantiate the view.
 	 *
 	 * @param   ModelInterface     $model     The model object.
-	 * @param   RendererInterface  $renderer  The renderer object.
-	 * @param   \SplPriorityQueue  $paths     The paths queue. [@deprecated 3.0]
+	 * @param   RendererInterface  $renderer  The renderer object. [@note Will be typehinted and required at 3.0]
+	 * @param   \SplPriorityQueue  $paths     The paths queue. [@deprecated Will be removed at 3.0]
 	 *
 	 * @since   1.0
 	 */
-	public function __construct(ModelInterface $model, RendererInterface $renderer, \SplPriorityQueue $paths = null)
+	public function __construct(ModelInterface $model, $renderer = null, \SplPriorityQueue $paths = null)
 	{
 		parent::__construct($model);
 
-		// Setup dependencies.
-		$this->paths    = isset($paths) ? $paths : $this->loadPaths();
-		$this->renderer = $renderer;
+		// Setup dependencies - B/C handling, check if $renderer is an instance of \SplPriorityQueue, remove at 3.0
+		if ($renderer instanceof \SplPriorityQueue)
+		{
+			$paths = $renderer;
+		}
+
+		$this->paths = isset($paths) ? $paths : $this->loadPaths();
+
+		// Set the renderer if we have it
+		if ($renderer instanceof RendererInterface)
+		{
+			$this->setRenderer($renderer);
+		}
 	}
 
 	/**
@@ -97,14 +116,32 @@ abstract class AbstractHtmlView extends AbstractView
 	}
 
 	/**
+	 * Retrieves the data array
+	 *
+	 * @return  array
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function getData()
+	{
+		return $this->data;
+	}
+
+	/**
 	 * Method to get the view layout.
 	 *
 	 * @return  string  The layout name.
 	 *
 	 * @since   1.0
+	 * @throws  \RuntimeException
 	 */
 	public function getLayout()
 	{
+		if (is_null($this->layout))
+		{
+			throw new \RuntimeException('The layout name is not set.');
+		}
+
 		return $this->layout;
 	}
 
@@ -117,7 +154,7 @@ abstract class AbstractHtmlView extends AbstractView
 	 * @return  mixed  The layout file name if found, false otherwise.
 	 *
 	 * @since   1.0
-	 * @deprecated  3.0  Use the path methods of RendererInterface instead
+	 * @deprecated  3.0  Set the paths in the RendererInterface object instead
 	 */
 	public function getPath($layout, $ext = 'php')
 	{
@@ -136,7 +173,7 @@ abstract class AbstractHtmlView extends AbstractView
 	 * @return  \SplPriorityQueue  The paths queue.
 	 *
 	 * @since   1.0
-	 * @deprecated  3.0  Use the path methods of RendererInterface instead
+	 * @deprecated  3.0  Set the paths in the RendererInterface object instead
 	 */
 	public function getPaths()
 	{
@@ -149,9 +186,15 @@ abstract class AbstractHtmlView extends AbstractView
 	 * @return  RendererInterface  The renderer object.
 	 *
 	 * @since   __DEPLOY_VERSION__
+	 * @throws  \RuntimeException
 	 */
 	public function getRenderer()
 	{
+		if (is_null($this->renderer))
+		{
+			throw new \RuntimeException('The renderer object is not set.');
+		}
+
 		return $this->renderer;
 	}
 
@@ -165,7 +208,23 @@ abstract class AbstractHtmlView extends AbstractView
 	 */
 	public function render()
 	{
-		return $this->renderer->render($this->layout);
+		return $this->getRenderer()->render($this->getLayout(), $this->getData());
+	}
+
+	/**
+	 * Sets the data array
+	 *
+	 * @param   array  $data  The data array.
+	 *
+	 * @return  AbstractHtmlView  Method supports chaining.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function setData(array $data)
+	{
+		$this->data = $data;
+
+		return $this;
 	}
 
 	/**
@@ -192,11 +251,27 @@ abstract class AbstractHtmlView extends AbstractView
 	 * @return  AbstractHtmlView  Method supports chaining.
 	 *
 	 * @since   1.0
-	 * @deprecated  3.0  Use the path methods of RendererInterface instead
+	 * @deprecated  3.0  Set the paths in the RendererInterface object instead
 	 */
 	public function setPaths(\SplPriorityQueue $paths)
 	{
 		$this->paths = $paths;
+
+		return $this;
+	}
+
+	/**
+	 * Sets the renderer object
+	 *
+	 * @param   RendererInterface  $renderer  The renderer object.
+	 *
+	 * @return  AbstractHtmlView  Method supports chaining.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function setRenderer(RendererInterface $renderer)
+	{
+		$this->renderer = $renderer;
 
 		return $this;
 	}
@@ -207,7 +282,7 @@ abstract class AbstractHtmlView extends AbstractView
 	 * @return  \SplPriorityQueue  The paths queue.
 	 *
 	 * @since   1.0
-	 * @deprecated  3.0  Use the path methods of RendererInterface instead
+	 * @deprecated  3.0  Set the paths in the RendererInterface object instead
 	 */
 	protected function loadPaths()
 	{
